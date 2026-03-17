62afe5d78ae97794bd95212863885fa8801c3144
Albert G. <252793@edu.p.lodz.pl>
2026-03-17 21:11:54 +0100

    Added a custom adapter that allows the use of custom line types.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,59 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+import javax.sound.sampled.Line;
+
+public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
+    public enum LineType {
+        BASIC, SPECIAL, DOTTED
+    }
+
+    private LineType lineType;
+    private int startX = 0, startY = 0;
+
+    public LineDrawerAdapter(LineType lineType) {
+        super();
+        this.lineType = lineType;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = null;
+
+        switch (lineType) {
+            case BASIC:
+                line = LineFactory.getBasicLine();
+                break;
+            case SPECIAL:
+                line = LineFactory.getSpecialLine();
+                break;
+            case DOTTED:
+                line = LineFactory.getDottedLine();
+                break;
+        }
+
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+
+        DrawerFeature.getDrawerController().drawLine(line);
+
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Drawer driver";
+    }
+}
